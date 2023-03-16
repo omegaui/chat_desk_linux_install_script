3fbf06e590fe3e1fca3036e07935962cca126154
omegaui <omegaui22@gmail.com>
2023-03-16 22:48:06 +0530

    🚀 Upgraded Shell script!

--- script/install-linux.sh
+++ script/install-linux.sh
@@ -23,10 +23,12 @@ echo -e "${CYAN}You need to be logged in to install it to ${BLUE}/opt${NC}"
 
 unzip "chat_desk*" -d chat_desk
 
-echo -e "${CYAN}Downloading ${BLUE}Core, App Icon and Desktop Entry${NC}"
+echo -e "${CYAN}Downloading ${BLUE}Core, executable, App Icon and Desktop Entry${NC}"
 
+sudo wget https://raw.githubusercontent.com/omegaui/chat_desk_core/main/script/chat_desk --output-document=/usr/bin/chat_desk
 sudo wget https://raw.githubusercontent.com/omegaui/chat_desk_core/main/bin/chat_desk_core.exe --output-document=chat_desk/chat_desk_core.exe
 sudo wget https://raw.githubusercontent.com/omegaui/chat_desk_core/main/pubspec.yaml --output-document=chat_desk/pubspec.yaml
+sudo chmod 777 /usr/bin/chat_desk
 sudo chmod 777 chat_desk/chat_desk_core.exe
 
 sudo wget "https://raw.githubusercontent.com/omegaui/chat_desk_linux_install_script/main/resources/app_icon.png" --output-document=/opt/chat_desk_icon.png
